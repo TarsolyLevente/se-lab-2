455248a8e572dff4a96fc5dfe0921347acb4d3bf
TarsolyLevente <tarsolyl@edu.bme.hu>
2023-10-18 09:48:38 +0200

    tests added

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,41 @@ class TorpedoStoreTest {
 
     // Assert
     assertEquals(true, result);
+  }
+
+  @Test
+  void single_fire_count(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(10);
+
+    // Act
+    store.fire(1);
+
+    // Assert
+    assertEquals(9, store.getTorpedoCount());
+  }
+
+  @Test
+  void more_fire_than_torpedos_exception(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(1);
+
+    // Act
+    Exception exception = assertThrows(IllegalArgumentException.class, ()-> store.fire(2));
+
+    // Assert
+    assertEquals("numberOfTorpedos", exception.getMessage());
+  }
+  
+  @Test
+  void number_of_torpedos_less_than_1_exception(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(0);
+
+    // Act
+    Exception exception = assertThrows(IllegalArgumentException.class, ()-> store.fire(1));
+
+    // Assert
+    assertEquals("numberOfTorpedos", exception.getMessage());
   }
 }
